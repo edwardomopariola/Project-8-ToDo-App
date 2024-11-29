fa77e8a395b69d0000ffc3f05c1eb3a348affc63
Edward Omopariola <femiedjr@gmail.com>
2024-11-29 11:30:12 -0500

    in progress

--- script.js
+++ script.js
@@ -3,26 +3,29 @@ let listContainer = document.getElementById("list-container"); //Declaring a var
 
 let myArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : []; //declaring a varible empty array
 
-console.log(myArray)
+// console.log(myArray)
 
 function saveData() {
     localStorage.setItem("items", JSON.stringify(myArray))
+    let myArray = myArray.filter((obj) => obj.id != todoObj);
 };
+
 
 function showList() {
 
     for(let i = 0 ; i < myArray.length; i++) {
-        let todoObj = myArray[i];
         
+        let todoObj = myArray[i];
         let li = document.createElement("li");
         li.innerHTML = todoObj.text;
-        li.id = todoObj.text
+        li.id = todoObj.id
         listContainer.appendChild(li);
         let span = document.createElement("span") //creating a span element
         span.innerHTML = "\u00d7" //
         li.appendChild(span);
     };
 };
+
 showList();
 
 function addTask() {
@@ -48,13 +51,18 @@ function addTask() {
 };
 
 listContainer.addEventListener("click", function(e) { 
-    if(e.target.tagName === "LI") { //when a user click on li, it will check the class list
-        e.target.classList.toggle("checked"); 
-        saveData()
-    }
+    // if(e.target.tagName === "LI") { //when a user click on li, it will check the class list
+    //     e.target.classList.toggle("checked"); 
+    //     saveData()
+    // }
+    console.log(e.target.getAttribute('data-value'))
 
-    else if (e.target.tagName === "SPAN") { //when a user click on span, it will delete the parent element
-        e.target.parentElement.remove()
-        saveData()
+
+    if (e.target.tagName === "SPAN") { //when a user click on span, it will delete the parent element
+        // console.log(e.target.getAttribute('data-value'))
+        // e.target.parentElement.remove()
+        // let localStorageData = localStorage.getItem("items")
+        // console.log(localStorageData)
+        // saveData()
     };
-}, false);
+});
